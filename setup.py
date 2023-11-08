ad291ef855626b4d687fab558a05409227612e25
Alexander Eichhorn <a.eichhorn@losjet.com>
2023-11-08 19:08:48 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="typegpt",
-    version="0.0.1",
+    version="0.1",
     author="Alexander Eichhorn",
     author_email="",
     description="TypeGPT - Make GPT safe for production",
