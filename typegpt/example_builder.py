d1e9e4f4e0923204053fc21b72d0776a6be82f9f
Alexander Eichhorn <a.eichhorn@losjet.com>
2024-02-09 14:42:40 +0100

    Implemented ExampleOutputFactory

--- typegpt/example_builder.py
+++ typegpt/example_builder.py
@@ -1,22 +1,57 @@
 from typegpt.base import BaseLLMResponse
-from typegpt.fields import LLMOutputInfo
-from typegpt.utils.type_checker import if_response_type
+from typegpt.fields import LLMArrayElementOutputInfo, LLMArrayOutputInfo, LLMOutputInfo
+from typegpt.utils.type_checker import if_response_type, is_response_type, if_array_element_list_type
 
 
 class ExampleOutputFactory:
-    def __init__(self, example: BaseLLMResponse):
+    def __init__(self, example: BaseLLMResponse, name_prefixes: list[str] = []):
         self.example = example
+        self.name_prefixes = name_prefixes
 
     def generate(self) -> str:
         lines: list[str] = []
 
         for field in self.example.__fields__.values():
+            field_name = " ".join(self.name_prefixes + [field.name])
+
             if isinstance(field.info, LLMOutputInfo):
-                if field_type := if_response_type(field.type_):
-                    subfields = list(field_type.__fields__.values())
-                    print(subfields)
+                value = getattr(self.example, field.key)
+
+                if is_response_type(field.type_):
+                    subelement_factory = ExampleOutputFactory(value, name_prefixes=self.name_prefixes + [field.name])
+                    lines.append("")  # add a newline before each subelement
+                    lines.append(subelement_factory.generate())
 
                 else:
-                    lines.append(f"{field.name}: {getattr(self.example, field.key)}")
+                    if value is None:
+                        continue
+                    lines.append(f"{field_name}: {value}")
 
-        return "\n".join(lines)
+            elif isinstance(field.info, LLMArrayOutputInfo):
+                if if_array_element_list_type(field.type_):
+                    lines.append("")
+                    for i, subelement in enumerate(getattr(self.example, field.key)):
+                        subelement_factory = ExampleOutputFactory(subelement, name_prefixes=self.name_prefixes + [field.name, str(i + 1)])
+                        lines.append(subelement_factory.generate())
+
+                else:
+                    for i, subelement in enumerate(getattr(self.example, field.key)):
+                        lines.append(f"{field_name} {i + 1}: {subelement}")
+
+            elif isinstance(field.info, LLMArrayElementOutputInfo):
+                value = getattr(self.example, field.key)
+
+                if is_response_type(field.type_):
+                    subelement_factory = ExampleOutputFactory(value, name_prefixes=self.name_prefixes + [field.name])
+                    lines.append("")
+                    lines.append(subelement_factory.generate())
+
+                else:
+                    if value is None:
+                        continue
+                    lines.append(f"{field_name}: {value}")
+
+            else:
+                print(f"Skipping field {field.name} because it's not an LLMOutputInfo. It is {field.info}.")
+
+        return "\n".join(lines).strip()
