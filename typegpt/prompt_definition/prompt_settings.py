4e60c45d2cf9119c3d3749aec52bcc82b511f3d3
Alexander Eichhorn <a.eichhorn@losjet.com>
2024-02-12 16:40:15 +0100

    Small doc change

--- typegpt/prompt_definition/prompt_settings.py
+++ typegpt/prompt_definition/prompt_settings.py
@@ -7,7 +7,7 @@ class PromptSettings:
     Prompt settings
 
     Args:
-        disable_formatting_instructions: Disables added system instruction that instructs LLM how to format output. Only use this if you use few-shot prompting or instruct the model yourself (not recommended).
+        disable_formatting_instructions: Disables added system instruction that instructs LLM how to format output. Only use this if you use few-shot prompting, a fine-tuned model, or instruct the model yourself (not recommended).
     """
 
     disable_formatting_instructions: bool = False
