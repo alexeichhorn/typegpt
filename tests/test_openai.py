4e4a25a459b14b73488a9a95d772f7c82a80cef6
Alexander Eichhorn <a.eichhorn@losjet.com>
2024-08-06 22:59:26 +0200

    added new gpt4o model

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -26,7 +26,7 @@ class TestOpenAIChatCompletion:
         ]
 
         # check if test covers all models (increase if new models are added)
-        assert len(OpenAIChatModel.__args__) == 23  #  type: ignore
+        assert len(OpenAIChatModel.__args__) == 24  #  type: ignore
 
         client = AsyncTypeOpenAI(api_key="mock")
 
@@ -51,12 +51,13 @@ class TestOpenAIChatCompletion:
         assert client.chat.completions.num_tokens_from_messages(test_messages, model="gpt-4-turbo-2024-04-09") == 27
         assert client.chat.completions.num_tokens_from_messages(test_messages, model="gpt-4o") == 26
         assert client.chat.completions.num_tokens_from_messages(test_messages, model="gpt-4o-2024-05-13") == 26
+        assert client.chat.completions.num_tokens_from_messages(test_messages, model="gpt-4o-2024-08-06") == 26
         assert client.chat.completions.num_tokens_from_messages(test_messages, model="gpt-4o-mini") == 26
         assert client.chat.completions.num_tokens_from_messages(test_messages, model="gpt-4o-mini-2024-07-18") == 26
 
     def test_max_token_counter(self):
         # check if test covers all models (increase if new models are added)
-        assert len(OpenAIChatModel.__args__) == 23  #  type: ignore
+        assert len(OpenAIChatModel.__args__) == 24  #  type: ignore
 
         client = AsyncTypeOpenAI(api_key="mock")
 
@@ -81,6 +82,7 @@ class TestOpenAIChatCompletion:
         assert client.chat.completions.max_tokens_of_model("gpt-4-turbo-2024-04-09") == 128_000
         assert client.chat.completions.max_tokens_of_model("gpt-4o") == 128_000
         assert client.chat.completions.max_tokens_of_model("gpt-4o-2024-05-13") == 128_000
+        assert client.chat.completions.max_tokens_of_model("gpt-4o-2024-08-06") == 128_000
         assert client.chat.completions.max_tokens_of_model("gpt-4o-mini") == 128_000
         assert client.chat.completions.max_tokens_of_model("gpt-4o-mini-2024-07-18") == 128_000
 
--- typegpt/openai/views.py
+++ typegpt/openai/views.py
@@ -23,6 +23,7 @@ OpenAIChatModel = Literal[
     "gpt-4-vision-preview",  # gpt-4 vision (preview)
     "gpt-4o",  # gpt-4o
     "gpt-4o-2024-05-13",
+    "gpt-4o-2024-08-06",
     "gpt-4o-mini",  # gpt-4o mini
     "gpt-4o-mini-2024-07-18",
 ]
